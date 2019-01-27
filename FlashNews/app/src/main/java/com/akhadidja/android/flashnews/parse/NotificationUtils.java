1e63d192c1b6046489e025c439b682512cfde32f
Khadidja <Khadidja@users.noreply.github.com>
2019-01-27 12:14:22 -0500

    updated project dependencies

--- FlashNews/app/src/main/java/com/akhadidja/android/flashnews/parse/NotificationUtils.java
+++ FlashNews/app/src/main/java/com/akhadidja/android/flashnews/parse/NotificationUtils.java
@@ -10,7 +10,7 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.os.Build;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
